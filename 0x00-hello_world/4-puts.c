40c247e3f8051fa1ab5be8ebc0779d955e0561ea
Cornelius_Ugwunwa <collinsonyeka23@gmail.com>
2022-09-08 10:54:21 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Short description, single line
- * Description: Longer description of the function)? 
- * Return: Description of the returned value
- */ 
-int main(void) 
+ * main - short description, one line
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
-
+	return (0);	
