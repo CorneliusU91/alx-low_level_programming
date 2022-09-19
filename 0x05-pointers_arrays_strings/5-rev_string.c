28b70dbf439917699c1a3406b75a3c02e7a47ed6
Cornelius_Ugwunwa <collinsonyeka23@gmail.com>
2022-09-19 13:25:57 -0700

    quick fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,9 +17,8 @@ void rev_string(char *s)
 
 	while (s[length1] != '\0')
 		length1++;
-	
-	length2 = length1 - 1
-	for (1 = 0; i < length1 /2; i++)
+	length2 = length1 - 1;
+	for (1 = 0; i < length1 / 2; i++)
 	{
 		"manalx" "xlanam"
 		tmp = s[i];
